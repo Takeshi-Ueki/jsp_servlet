b3840cbfe4d920c047d68da49cc7ae2d7d7b3d81
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-10-19 17:16:02 +0900

    10/19 work

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+// HelloWorld Servlet
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            out.println("<html><head>paiza</head><body>");
+            out.println("<p>Hello paiza!</p>");
+            out.println("</body></html>");
+    }
+}
